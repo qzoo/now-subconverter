3f46790bb2b76bdcb78ad54d6101d1fd296a8101
amy <37330274+qzoo@users.noreply.github.com>
2025-02-28 21:29:29 +0800

    Update server.js

--- subconverter/server.js
+++ subconverter/server.js
@@ -3,11 +3,16 @@ const path = require('path');
 
 const subconverterPath = path.join(__dirname, 'subconverter.exe');
 
+// 启动 subconverter.exe
 exec(subconverterPath, (err, stdout, stderr) => {
     if (err) {
-        console.error(err);
+        console.error('Failed to start subconverter:', err);
         return;
     }
-    console.log(stdout);
-    console.error(stderr);
+    console.log('subconverter started:', stdout);
 });
+
+// 导出一个符合 Vercel 要求的函数
+module.exports = (req, res) => {
+    res.status(200).send('Subconverter is running!');
+};
