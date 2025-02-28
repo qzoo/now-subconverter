7109b5321a1e58949531c6752606b743de44a8b3
GitHub Action <noreply@github.com>
2025-02-28 10:05:55 +0000

    Update subconverter

--- subconverter/shim.js
+++
@@ -1,7 +0,0 @@
-const { exec } = require('child_process');
-module.exports = async (req, res) => {
-  const converter = exec('./subconverter', (error) => {
-    if (error) throw error;
-  });
-  converter.stdout.pipe(res);
-};
